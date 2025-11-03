c43fe7042cfc341f5e4d66631996db70f5108427
AJEGUM <juliandguevara12@gmail.com>
2025-11-03 00:56:35 -0500

    Ahora se muestran los productos de la BD en la pagina principal, ademas si esta inactivo se bloquea el producto para comprar

--- frontend/src/app/components/nav-bar/nav-bar.ts
+++ frontend/src/app/components/nav-bar/nav-bar.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Footer } from '../footer/footer';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterModule, Footer],
+  imports: [RouterModule, Footer, CommonModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
 export class NavBar {
+  menuOpen: boolean = false;
 
+  abrirmenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  cerrarmenu() {
+    this.menuOpen = false;
+  }
 }
--- frontend/src/app/pages/pag-principal/pag-principal.ts
+++ frontend/src/app/pages/pag-principal/pag-principal.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductosService } from '../../services/productosServices/productos-services';
 
 @Component({
   selector: 'app-pag-principal',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './pag-principal.html',
-  styles: ``,
 })
-export class PagPrincipal {
+export class PagPrincipal implements OnInit {
 
+  productos: any[] = [];
+
+  constructor(private productosService: ProductosService) {}
+
+  ngOnInit() {
+    this.cargarProductos();
+  }
+
+  cargarProductos() {
+    this.productosService.obtenerProductos().subscribe(data => {
+      this.productos = data;
+      console.log("PRODUCTOS CARGADOS:", this.productos);
+    });
+  }
+
+  obtenerImagen(prod: any) {
+    if (!prod.imagen) return 'assets/no-image.png';
+
+    // Si viene base64 crudo
+    if (!prod.imagen.startsWith('data:image')) {
+      return 'data:image/jpeg;base64,' + prod.imagen;
+    }
+
+    // Si ya viene completo
+    return prod.imagen;
+  }
 }
