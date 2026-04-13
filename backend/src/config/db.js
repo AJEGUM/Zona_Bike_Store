70e80633cf022d65cb99918d514c1df7f1096622
Julián Guevara <tu-correo@ejemplo.com>
2026-04-12 23:25:22 -0500

    necesario

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,18 +1,18 @@
-// // db.js - Simulación temporal en memoria
-const sessions = {};
+// db.js - Simulación temporal en memoria
+// const sessions = {};
 
-const db = {
-    usuarios: {
-        update: (data) => ({
-            where: (query) => {
-                const phone = query.phone;
-                sessions[phone] = { ...sessions[phone], ...data };
-                console.log(`[DB Mock] Usuario ${phone} actualizado a contexto: ${data.context}`);
-            }
-        }),
-        get: (phone) => sessions[phone] || { context: null }
-    }
-};
+// const db = {
+//     usuarios: {
+//         update: (data) => ({
+//             where: (query) => {
+//                 const phone = query.phone;
+//                 sessions[phone] = { ...sessions[phone], ...data };
+//                 console.log(`[DB Mock] Usuario ${phone} actualizado a contexto: ${data.context}`);
+//             }
+//         }),
+//         get: (phone) => sessions[phone] || { context: null }
+//     }
+// };
 
 // module.exports = db;
 
